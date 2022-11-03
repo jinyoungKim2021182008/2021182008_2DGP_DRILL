69346f7870855957595777a92065f85dbb386626
jinyoungKim2021182008 <rlxo8749@naver.com>
2022-11-03 15:10:13 +0900

    Drill12 완성
    2021182008 김진영

---
+++ Drill 12/grass.py
@@ -0,0 +1,12 @@
+from pico2d import *
+
+class Grass:
+    def __init__(self, x, y):
+        self.image = load_image('grass.png')
+        self.x, self.y = x, y
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
